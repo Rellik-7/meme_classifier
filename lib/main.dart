862931beaa576e9843014248910700b89b6737ac
dazyy <dazyyadavgs@gmail.com>
2022-06-07 21:00:12 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -58,32 +59,47 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Container(
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 1.0),
+      child: Container(
 
-        decoration:  BoxDecoration(
-          image: DecorationImage(
-              image: const AssetImage('assets/images/Screenshot (121).png'),
-              fit: BoxFit.cover,
-            colorFilter: ColorFilter.mode(Colors.lightBlueAccent.withOpacity(0.2), BlendMode.darken),
-              
+          decoration:  BoxDecoration(
+            image: DecorationImage(
+                image: const AssetImage('assets/images/Screenshot (121).png'),
+                fit: BoxFit.cover,
+              colorFilter: ColorFilter.mode(Colors.lightBlueAccent.withOpacity(0.2), BlendMode.darken),
 
+
+            ),
+          ),
+
+        child:  Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            title: const Text('MemeClassifier App'),
+            centerTitle: true,
+          ),
+          body: Center(
+
+
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 200.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  AnimatedTextKit(animatedTexts: [
+                   WavyAnimatedText('Welcome',textStyle: const TextStyle(fontSize: 30,fontStyle: FontStyle.italic)),
+
+                    
+                  ]),
+
+                  ElevatedButton(onPressed: (){}, child: const Text('Get Started ->')),
+                ],
+              ),
+            ),
           ),
         ),
-      child:  Scaffold(
-        backgroundColor: Colors.transparent,
-        appBar: AppBar(
-          title: const Text('MemeClassifier App'),
-          centerTitle: true,
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(onPressed: (){}, child: const Text('Get Started')),
-            ],
-          ),
-        ),
-      ),
-      );
+    );
   }
 }
